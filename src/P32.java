de2a04a2c14867f4672dfde36e4baff78aaf7a14
Alex <alex.sun@oocl.com>
2013-02-06 15:10:10 +0800

    enrich P32

--- src/P32.java
+++ src/P32.java
@@ -2,9 +2,10 @@
 public class P32 {
     
     public static void main(String... args) {
+    	System.out.println(new Integer(1) & new Integer(0)); //auto-unboxing, primitives' and.
         System.out.println(new Integer(1) > new Integer(0)); //auto-unboxing, primitives' comparison.
-        System.out.println(new Integer(0) == new Integer(0)); //object comparison.
-        System.out.println(new Integer(0) != new Integer(0)); //object comparison.
+        System.out.println(new Integer(0) == new Integer(0)); //won't auto-unboxing, just object comparison.
+        System.out.println(new Integer(0) != new Integer(0)); //won't auto-unboxing, just object comparison.
         System.out.println(new Integer(0) == 0); //auto-unboxing, primitives' comparison.
         System.out.println(0 == new Integer(0)); //auto-unboxing, primitives' comparison.
         System.out.println(Integer.valueOf(127) == Integer.valueOf(127)); //object comparison. hit cache(-128~127).
